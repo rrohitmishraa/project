413e0b34761c12c58463e5bf1e9e1fbc7567f1bf
rrohitmishraa <47442598+rrohitmishraa@users.noreply.github.com>
2019-04-16 08:54:25 -0700

    this is the first version

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>HOME PAGE</title>
+</head>
+<style>
+	img:hover
+	{
+		-webkit-transform:scaleX(-1);
+		transform: scaleX(-1);
+	}
+</style>
+<body bgcolor="#633974">
+<center>
+	<a href="fac_login.php" ><img src="img/fac.png"></a>
+	<a href="stu_login.php"><img src="img/stu.png"></a>
+</center>
+</body>
+</html> 
